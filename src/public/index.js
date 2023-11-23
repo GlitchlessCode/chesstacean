ff973e5e754adf7d21a8d2fde5ae664030f50b59
Jaiden Appiah <j.appiah@EDU.epsb.ca>
2023-11-23 14:52:10 -0700

    navbar start, favicon start

--- src/public/index.js
+++ src/public/index.js
@@ -1,15 +1,1 @@
-/** @type {HTMLCanvasElement} */
-const cnv = document.getElementById("chessboard");
-const ctx = cnv.getContext("2d");
 
-const size = 8;
-
-// draw vertical board lines
-
-const lineWidth = 2;
-const spacing = cnv.width - (lineWidth + lineWidth * size) / size;
-
-for (let i = 0; i < size; i++) {
-}
-
-// draw horizontal board lines
