93ebd78e18fdeaa61e8050e357773d53ab8ab0c3
GlitchlessCode <timothy.veugelers@gmail.com>
2023-11-24 14:54:51 -0700

    Figuring out WebSockets

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+use futures_util::{FutureExt, StreamExt};
 use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let first = warp::path("game").and(warp::path::end()).map(|| "Hello!");
-    let second = warp::any().map(|| {
-        Response::builder().status(404).body(
-            "<!DOCTYPE html><html><head></head><body><h1>404 Page Not Found</h1></body></html>",
-        )
-    });
-    let test = first.or(second);
+    // let first = warp::path("game").and(warp::path::end()).map(|| "Hello!");
+    // let second = warp::any().map(|| {
+    //     Response::builder().status(404).body(
+    //         "<!DOCTYPE html><html><head></head><body><h1>404 Page Not Found</h1></body></html>",
+    //     )
+    // });
+    // let test = first.or(second);
 
-    warp::serve(test).run(([127, 0, 0, 1], 3000)).await;
+    // warp::serve(test).run(([127, 0, 0, 1], 3000)).await;
+
+    let routes = warp::path("echo").and(warp::ws()).map(|ws: warp::ws::Ws| {
+        ws.on_upgrade(|websocket| {
+            let (tx, rx) = websocket.split();
+            rx.forward(tx).map(|result| {
+                if let Err(e) = result {
+                    eprintln!("websocket error: {:?}", e)
+                }
+            })
+        })
+    });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
 }
