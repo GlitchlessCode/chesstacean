f2cc1900109df8b7ec34403a12317b3acd65f3a6
GlitchlessCode <timothy.veugelers@gmail.com>
2023-11-27 14:52:29 -0700

    Added Argon2, Experimenting with combined filters

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use futures_util::{FutureExt, StreamExt};
-use warp::{http::Response, Filter};
+use futures_util::{FutureExt, SinkExt, StreamExt, TryFutureExt};
+use warp::{
+    filters::ws::{Message, WebSocket},
+    http::Response,
+    Filter,
+};
 
 #[tokio::main]
 async fn main() {
-    // let first = warp::path("game").and(warp::path::end()).map(|| "Hello!");
-    // let second = warp::any().map(|| {
-    //     Response::builder().status(404).body(
-    //         "<!DOCTYPE html><html><head></head><body><h1>404 Page Not Found</h1></body></html>",
-    //     )
-    // });
-    // let test = first.or(second);
+    let first = warp::get().and(warp::path("game").and(warp::path::end()).map(|| "Hello!"));
+    let second = warp::get().and(warp::any().map(|| {
+        Response::builder().status(404).body(
+            "<!DOCTYPE html><html><head></head><body><h1>404 Page Not Found</h1></body></html>",
+        )
+    }));
 
-    // warp::serve(test).run(([127, 0, 0, 1], 3000)).await;
+    let ws = warp::path("echo")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket| handle_connected(socket)));
 
-    let routes = warp::path("echo").and(warp::ws()).map(|ws: warp::ws::Ws| {
-        ws.on_upgrade(|websocket| {
-            let (tx, rx) = websocket.split();
-            rx.forward(tx).map(|result| {
-                if let Err(e) = result {
-                    eprintln!("websocket error: {:?}", e)
-                }
-            })
-        })
-    });
+    let routes = ws.or(first).or(second);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
+}
+
+async fn handle_connected(socket: WebSocket) {
+    let (mut tx, mut rx) = socket.split();
+    tx.send(Message::text("Connected!"))
+        .unwrap_or_else(|e| eprintln!("Websocket Send Error: {}", e))
+        .await
 }
