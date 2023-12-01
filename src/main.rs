de1fdbe1ea980be18d94334c35ee195ba62a164a
GlitchlessCode <timothy.veugelers@gmail.com>
2023-12-01 14:55:30 -0700

    Graceful Shutdown Experiments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use chesstacean::{chess, server};
+use std::{
+    io,
+    net::{Ipv4Addr, SocketAddrV4},
+};
+
+use chesstacean::server;
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let tx = server::start(server::ServerConfig {
+        addr: SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 3000),
+    })
+    .await;
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    tx.send(()).unwrap();
+}
--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,27 @@
+use std::net::SocketAddrV4;
 
+use tokio::sync::oneshot::{self, Sender};
+use warp::Filter;
+
+pub async fn start(config: ServerConfig) -> Sender<()> {
+    let routes = warp::any().map(|| "Hello, World!");
+
+    let (tx, rx) = oneshot::channel::<()>();
+
+    let (addr, server) = warp::serve(routes).bind_with_graceful_shutdown(config.addr, async {
+        rx.await.ok();
+    });
+
+    // Spawn the server into a runtime
+    tokio::task::spawn(server);
+
+    tx
+}
+
+pub struct ServerConfig {
+    pub addr: SocketAddrV4,
+}
+
+impl ServerConfig {
+    pub fn new() {}
+}
