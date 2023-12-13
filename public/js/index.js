5e26c53889de1ddadb5ae06e3a016dc48a3397b9
somnxd <jaidenappiah@gmail.com>
2023-12-13 14:56:05 -0700

    canvas lines start

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 import "./modules/ca.chesstacean.components.js";
 import "./web-components/registry.js";
 
@@ -21,3 +22,50 @@ window.closeGameWindow = () => {
 	nav.removeAttribute('style');
 	main.removeAttribute('style');
 }
+
+/** @type {HTMLCanvasElement} */
+const cnv = document.getElementById("game-board");
+const ctx = cnv.getContext('2d');
+
+requestAnimationFrame(update);
+
+const borderWidth = 2;
+const gridWidth = 8;
+const gridHeight = 8;
+
+function update() {
+	ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+	// draw borders
+
+	drawLine(borderWidth / 2, 0, 0, cnv.height);
+	drawLine(0, borderWidth / 2, cnv.width , 0);
+	drawLine(cnv.width - borderWidth / 2, 0, cnv.width, cnv.height);
+	drawLine(0, cnv.height - borderWidth / 2, cnv.width, cnv.height);
+
+	for (let i = 0; i < gridWidth - 1; i++) {
+		const x = cnv.width / gridWidth * (i + 1);
+
+		drawLine(x, 0, x, cnv.height);
+	}
+
+	requestAnimationFrame(update);
+}
+
+function drawLine(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+
+	ctx.strokeStyle = "#666666";
+	ctx.lineWidth   = 2;
+	ctx.stroke();
+};
+
+cnv.addEventListener("resize", () => {
+	cnv.width  = Math.floor(cnv.getBoundingClientRect().width  * window.devicePixelRatio);
+	cnv.height = Math.floor(cnv.getBoundingClientRect().height * window.devicePixelRatio);
+});
+
+cnv.dispatchEvent(new Event("resize"));
+
