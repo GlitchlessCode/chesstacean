7c711b4d0be93e2b8570f84c8557e181d1be8b68
codingcolonel <ethanvink2006@gmail.com>
2023-12-13 12:19:35 -0700

    started adding some piece structure
    (yesterday's commit)

--- src/chess.rs
+++ src/chess.rs
@@ -1,1 +1,35 @@
 
+
+mod pieces {
+    enum TeamColor {
+        White,
+        Black
+    }
+
+    enum PieceType {
+        King,
+        Queen,
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+    }
+    pub struct Piece {
+        color: TeamColor,
+        coordinates: (i16, i16),
+        piece_type: PieceType,
+    }
+
+    impl Piece {
+        fn get_value(self) -> i16 {
+            match self.piece_type {
+                King => 100,
+                Queen => 9,
+                Pawn => 1,
+                Bishop => 3,
+                Knight => 3,
+                Rook => 5,
+            }
+        }
+    }
+}
