2bcff4b8951ea94759b8c98a520913b62bb52568
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-12-30 17:11:31 -0700

    added board constructor
    gonna add the position for pieces later

--- src/chess/game/pieces.rs
+++ src/chess/game/pieces.rs
@@ -2,125 +2,143 @@ use std::rc::Weak;
 use std::sync::Arc;
 
 pub enum Piece {
-    King {
-        color: TeamColor,
-        position: Position,
-    },
-    Queen{
-        color: TeamColor,
-        position: Position,
-    },
-    Pawn{
-        color: TeamColor,
-        position: Position,
-    },
-    Bishop{
-        color: TeamColor,
-        position: Position,
-    },
-    Knight{
-        color: TeamColor,
-        position: Position,
-    },
-    Rook{
-        color: TeamColor,
-        position: Position,
-    },
+    King { color: TeamColor, position: Position },
+    Queen { color: TeamColor, position: Position },
+    Pawn { color: TeamColor, position: Position },
+    Bishop { color: TeamColor, position: Position },
+    Knight { color: TeamColor, position: Position },
+    Rook { color: TeamColor, position: Position },
 }
 
 impl Piece {
     // get color name
     pub fn get_color(self) -> TeamColor {
         match self {
-            Piece::King {color:color,position:_} => color,
-            Piece::Queen {color:color,position:_}  => color,
-            Piece::Pawn {color:color,position:_}  => color,
-            Piece::Bishop {color:color,position:_}  => color,
-            Piece::Knight {color:color,position:_}  => color,
-            Piece::Rook {color:color,position:_}  => color,
+            Piece::King {
+                color: color,
+                position: _,
+            } => color,
+            Piece::Queen {
+                color: color,
+                position: _,
+            } => color,
+            Piece::Pawn {
+                color: color,
+                position: _,
+            } => color,
+            Piece::Bishop {
+                color: color,
+                position: _,
+            } => color,
+            Piece::Knight {
+                color: color,
+                position: _,
+            } => color,
+            Piece::Rook {
+                color: color,
+                position: _,
+            } => color,
         }
     }
 
     // get position
     pub fn get_position(self) -> Position {
         match self {
-            Piece::King {color:_,position:position} => position,
-            Piece::Queen {color:_,position:position}  => position,
-            Piece::Pawn {color:_,position:position}  => position,
-            Piece::Bishop {color:_,position:position}  => position,
-            Piece::Knight {color:_,position:position}  => position,
-            Piece::Rook {color:_,position:position}  => position,
+            Piece::King {
+                color: _,
+                position: position,
+            } => position,
+            Piece::Queen {
+                color: _,
+                position: position,
+            } => position,
+            Piece::Pawn {
+                color: _,
+                position: position,
+            } => position,
+            Piece::Bishop {
+                color: _,
+                position: position,
+            } => position,
+            Piece::Knight {
+                color: _,
+                position: position,
+            } => position,
+            Piece::Rook {
+                color: _,
+                position: position,
+            } => position,
         }
     }
 
     // get material value
     pub fn get_value(self) -> i16 {
         match self {
-            Piece::King {color:_,position:_} => 100,
-            Piece::Queen {color:_,position:_}  => 9,
-            Piece::Pawn {color:_,position:_}  => 1,
-            Piece::Bishop {color:_,position:_}  => 3,
-            Piece::Knight {color:_,position:_}  => 3,
-            Piece::Rook {color:_,position:_}  => 5,
+            Piece::King { color: _, position: _ } => 100,
+            Piece::Queen { color: _, position: _ } => 9,
+            Piece::Pawn { color: _, position: _ } => 1,
+            Piece::Bishop { color: _, position: _ } => 3,
+            Piece::Knight { color: _, position: _ } => 3,
+            Piece::Rook { color: _, position: _ } => 5,
         }
     }
 
     // get moveset for each piece
     pub fn get_moveset(self) {
         match self {
-            Piece::King {color:_,position:_}  => (),
-            Piece::Queen {color:_,position:_}  => (),
-            Piece::Pawn {color:_,position:_}  => (),
-            Piece::Bishop {color:_,position:_}  => (),
-            Piece::Knight {color:_,position:_}  => (),
-            Piece::Rook {color:_,position:_}  => (),
+            Piece::King { color: _, position: _ } => (),
+            Piece::Queen { color: _, position: _ } => (),
+            Piece::Pawn { color: _, position: _ } => (),
+            Piece::Bishop { color: _, position: _ } => (),
+            Piece::Knight { color: _, position: _ } => (),
+            Piece::Rook { color: _, position: _ } => (),
         }
     }
 
     // piece type functions
     pub fn is_king(self) -> bool {
         match self {
-            Piece::King {color:_,position:_}  => true,
-            _ => false
+            Piece::King { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
     pub fn is_queen(self) -> bool {
         match self {
-            Piece::Queen {color:_,position:_}  => true,
-            _ => false
+            Piece::Queen { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
     pub fn is_pawn(self) -> bool {
         match self {
-            Piece::Pawn {color:_,position:_}  => true,
-            _ => false
+            Piece::Pawn { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
     pub fn is_bishop(self) -> bool {
         match self {
-            Piece::Bishop {color:_,position:_}  => true,
-            _ => false
+            Piece::Bishop { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
     pub fn is_knight(self) -> bool {
         match self {
-            Piece::Knight {color:_,position:_}  => true,
-            _ => false
+            Piece::Knight { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
     pub fn is_rook(self) -> bool {
         match self {
-            Piece::Rook {color:_,position:_}  => true,
-            _ => false
+            Piece::Rook { color: _, position: _ } => true,
+            _ => false,
         }
     }
 
-    // 
+    //
 
     // pub fn get_moves(self) -> Vec<Vec<MoveList>> {
 
@@ -129,7 +147,7 @@ impl Piece {
     pub fn get_valid_moves(self) {
         //    let moves = self.get_moves();
     }
-    
+
     pub fn can_move(self, id: i16) -> bool {
         // check if tile is one of the possible moves in Piece.get_moves
         false
@@ -152,7 +170,7 @@ enum Progress<T> {
     Invalid(T),
 }
 
-enum TeamColor {
+pub enum TeamColor {
     White,
     Black,
 }
